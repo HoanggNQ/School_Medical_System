88a34af35382114e1869d090cf8871fcd4803844
ontrip <hoangtoc202@gmail.com>
2025-07-11 18:00:24 +0700

    fix vac consent

--- backend/School_Medical_System/src/main/java/sms/swp391/controllers/HealthCheckConsentController.java
+++ backend/School_Medical_System/src/main/java/sms/swp391/controllers/HealthCheckConsentController.java
@@ -206,7 +206,7 @@ public class HealthCheckConsentController {
     @Operation(
             summary = "Lấy tất cả consents theo campaignId với phân trang và tìm kiếm"
     )
-    @GetMapping("/getAll")
+    @GetMapping("/campaigns/{campaignId}/consentsAll/")
     public ResponseEntity<ResponseObject> getAll(
             @RequestParam(value = "campaignId", required = false) Long campaignId,
             @RequestParam(value = "search",     required = false) String search,
