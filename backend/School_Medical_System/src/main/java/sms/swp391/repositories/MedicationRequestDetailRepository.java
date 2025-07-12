c9c115dfad9813467bcf5271d4df969b7ed8ad15
ontrip <hoangtoc202@gmail.com>
2025-07-12 21:50:01 +0700

    update medication request for parent

--- backend/School_Medical_System/src/main/java/sms/swp391/repositories/MedicationRequestDetailRepository.java
+++ backend/School_Medical_System/src/main/java/sms/swp391/repositories/MedicationRequestDetailRepository.java
@@ -3,9 +3,11 @@ package sms.swp391.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sms.swp391.models.entities.MedicationRequestDetailEntity;
+import sms.swp391.models.entities.MedicationRequestEntity;
 
 import java.util.List;
 
 @Repository
 public interface MedicationRequestDetailRepository extends JpaRepository<MedicationRequestDetailEntity, Long> {
+    void deleteAllByRequest(MedicationRequestEntity request);
 }
