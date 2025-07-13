1f66b8ef4ec5abbbf13e8f990a60a18d1c5fbf69
ontrip <hoangtoc202@gmail.com>
2025-07-13 11:17:03 +0700

    fix medical req

--- backend/School_Medical_System/src/main/java/sms/swp391/repositories/MedicationRequestDetailRepository.java
+++ backend/School_Medical_System/src/main/java/sms/swp391/repositories/MedicationRequestDetailRepository.java
@@ -1,13 +1,23 @@
 package sms.swp391.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sms.swp391.models.entities.MedicationRequestDetailEntity;
 import sms.swp391.models.entities.MedicationRequestEntity;
 
 import java.util.List;
 
+
 @Repository
 public interface MedicationRequestDetailRepository extends JpaRepository<MedicationRequestDetailEntity, Long> {
-    void deleteAllByRequest(MedicationRequestEntity request);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM MedicationRequestDetailEntity d WHERE d.request = :request")
+    void deleteAllByRequest(@Param("request") MedicationRequestEntity request);
+
+    List<MedicationRequestDetailEntity> findByRequest(MedicationRequestEntity request);
 }
