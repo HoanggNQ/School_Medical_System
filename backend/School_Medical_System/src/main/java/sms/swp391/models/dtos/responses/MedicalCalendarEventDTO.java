4828c76abd3cec0163762c54019f468cb6009144
ontrip <hoangtoc202@gmail.com>
2025-07-14 15:17:04 +0700

    api show calendar

--- backend/School_Medical_System/src/main/java/sms/swp391/models/dtos/responses/MedicalCalendarEventDTO.java
+++ backend/School_Medical_System/src/main/java/sms/swp391/models/dtos/responses/MedicalCalendarEventDTO.java
@@ -9,7 +9,12 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class MedicalCalendarEventDTO {
+    private Long eventId;           // ID của chiến dịch (nếu có)
+    private String name;               // Tên chiến dịch/sự kiện
+    private String location;           // Địa điểm (nếu có)
+
     private LocalDate startDate;
-    private LocalDate endDate;   // Nếu không có thì FE dùng startDate
-    private String type;         // VACCINATION, HEALTH_CHECK, MEDICATION, CONSULTATION
+    private LocalDate endDate;         // Nếu không có thì FE dùng startDate
+    private String type;               // VACCINATION, HEALTH_CHECK, MEDICATION, CONSULTATION
 }
+
