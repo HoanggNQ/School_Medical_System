3473d83f3c6ae915963257820e1a274e644dec2d
ontrip <hoangtoc202@gmail.com>
2025-07-28 12:04:07 +0700

    vali hẹn tư vấn

--- backend/School_Medical_System/src/main/java/sms/swp391/models/entities/HealthConsultationScheduleEntity.java
+++ backend/School_Medical_System/src/main/java/sms/swp391/models/entities/HealthConsultationScheduleEntity.java
@@ -20,6 +20,9 @@ public class HealthConsultationScheduleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nurse_id", nullable = false)
+    private UserEntity nurse;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id", nullable = false)
