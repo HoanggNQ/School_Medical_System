fa4366bb41008dbea86ca50b630c6da98a5c75c7
caokien99 <caokien0971028169@gmail.com>
2025-07-28 11:46:24 +0700

    finally

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -72,6 +72,7 @@ const Sidebar = ({ activeTab, setActiveTab }) => {
           { type: 'item', id: 'event', label: 'Sự kiện y tế', icon: ClipboardList, path: '/event' },
                     // { type: 'item', id: 'heath-result', label: 'Kết quả sức khỏe', icon: ClipboardList, path: '/heath-result' },
           // { type: 'item', id: 'vaccine-result', label: 'Kết quả tiêm chủng', icon: ClipboardList, path: '/vaccine-result' },
+          { type: 'item', id: 'consultation-schedules-nurse', label: 'Lịch tư vấn', icon: ClipboardList, path: '/consultation-schedules' },
           { type: 'item', id: 'HealthDeclarationSearch', label: 'Phiếu sức khỏe', icon: ClipboardList, path: '/HealthDeclarationSearch' }
         ];
       case 'STUDENT':
